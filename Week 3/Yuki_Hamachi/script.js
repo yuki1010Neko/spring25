4faae7c263e3030154265543078f7d5413597015
yuki1010Neko <yuki1010@berkeley.edu>
2025-02-20 20:23:59 -0800

    I tried to add a heart mark that is created when the cookie is clicked but it failed....

---
+++ Week 3/Yuki_Hamachi/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded",  function () {
+    function addOneToCounter(event){
+   
+        let counterElement=document.getElementById("counter");
+        counterElement.innerText=parseInt(counterElement.innerText)+1
+
+        let heart=document.createElement("div");
+        heart.innerHTML="❤️";
+        heart.style.position="absolute";
+        heart.style.left=event.clientX+"px";
+        heart.style.top=event.clientY+"px";
+        heart.style.fontSize="24px";
+        heart.style.opacity="1";
+        heart.style.transition="all 1s ease-out";
+        heart.style.pointerEvents="none";
+   
+        document.body.appendChild(heart);
+
+        setTimeout(()=>{
+            heart.style.transform="translateY(-50px)";
+            heart.style.opacity="0";
+        },100);
+
+        setTimeout(()=>{
+            heart.remove();
+        }, 1000);
+    
+        document.getElementById("cookieImg").addEventListener("click", addOneToCounter);
+
+       
+};
